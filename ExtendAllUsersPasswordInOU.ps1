21438d34ec4dcfdd44140879a7777bac14fef16f
Jairo Jimenez <jairo95j@gmail.com>
2020-10-13 20:02:52 -0400

    update

--- ExtendAllUsersPasswordInOU.ps1
+++ ExtendAllUsersPasswordInOU.ps1
@@ -1,14 +1,20 @@
-﻿#################################################################################################################
+
+#################################################################################################################
 #                                                                                                               #
 # Version 1.0 October 2020                                                                                      #
 # Jairo Jimenez Arenas                                                                                          #
 #                                                                                                               #
-# Script to get Members in a group from an OU                                                                   #
+# Script to Extend All Users in an OUs password                                                                 #
 # Requires: Windows PowerShell Module for Active Directory                                                      #
 #                                                                                                               #
-##################################################################################################################
+#################################################################################################################
 
 Get-ADOrganizationalUnit -Filter * | Select Name, DistinguishedName | Out-String
 $OUSearchBase = Read-Host -Prompt 'Copy & Paste OU'
-$GroupName = Read-Host -Prompt 'Name of Security/Distro Group'
-Get-ADUser -Filter * -Properties Name, MemberOf -SearchBase $OUSearchBase| Where{$_.MemberOf -contains (Get-ADGroup $GroupName).distinguishedName} | Select Name
+
+$Users = get-aduser -Filter "(Enabled -eq 'True') -And (ObjectClass -eq 'user') -And (PasswordNeverExpires -eq 'False')" -SearchBase $OUSearchBase | % {
+
+    Set-ADUser -Identity $_ -Replace @{pwdLastSet=0} 
+
+    Set-ADUser -Identity $_ -Replace @{pwdLastSet=-1} 
+}
